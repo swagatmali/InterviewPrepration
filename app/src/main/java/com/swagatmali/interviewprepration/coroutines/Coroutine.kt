2816d9ff955698f2b4601fa70f554a1c68ea6083
swagathome <swagatmali88@gmail.com>
2026-01-13 19:38:47 +0530

    Coroutine solution

--- app/src/main/java/com/swagatmali/interviewprepration/coroutines/Coroutine.kt
+++ app/src/main/java/com/swagatmali/interviewprepration/coroutines/Coroutine.kt
@@ -1,10 +1,18 @@
 package com.swagatmali.interviewprepration.coroutines
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 //Main routine
 fun main() {
     first() // Subroutine
     second() // Subroutine
 }
+
+// Stack
+//Main -> First
+// Here program flow is blocked by First as it will never end. We have to make first as non blocking.
 
 fun first() {
     var first = 10
@@ -16,6 +24,34 @@ fun first() {
 fun second() {
     var second = 0
     while (true) {
-        println("First ${second++}")
+        println("Second ${second++}")
     }
 }
+
+//Solution
+
+//Main routine
+//fun main(): Unit = runBlocking {
+//    launch {
+//        first1() // Coroutine
+//    }
+//    launch {
+//        second1() // Coroutine
+//    }
+//}
+//
+//suspend fun first1() {
+//    var first = 10
+//    while (true) {
+//        println("First ${first++}")
+//        delay(100)
+//    }
+//}
+//
+//suspend fun second1() {
+//    var second = 0
+//    while (true) {
+//        println("Second ${second++}")
+//        delay(100)
+//    }
+//}
